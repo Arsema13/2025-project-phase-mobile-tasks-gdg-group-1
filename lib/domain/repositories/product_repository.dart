ec350c94332d3ff43eaff2ba2c32304ebffea0ac
Arsema13 <arsematewodros123@gmail.com>
2025-03-20 14:40:42 +0300

    commit

--- lib/domain/repositories/product_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/product.dart';
-
-abstract class ProductRepository {
-  Future<List<Product>> getAllProducts(); // Get all products
-  Future<Product?> getProductById(String id); // Get product by ID
-  Future<void> addProduct(Product product); // Create a new product
-  Future<void> updateProduct(Product product); // Update a product
-  Future<void> deleteProduct(String id); // Delete a product
-}
