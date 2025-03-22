51ea6ca1d2f22abf4ff2e3e4474784f5afe8048c
Arsema13 <arsematewodros123@gmail.com>
2025-03-22 21:38:26 +0300

    updated

--- Task 4/ecom/lib/path/mock_product_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mockito/mockito.dart';
-import 'package:ecommerce/domain/data/product_repository.dart';
-
-// Mock class for ProductRepository
-class MockProductRepository extends Mock implements ProductRepository {}
